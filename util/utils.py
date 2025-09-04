57824bdac15a56e29cf3816b2febbffd055f6710
tdquang <tdquang7@gmail.com>
2025-09-04 09:09:00 +0700

    remove max_batch_size

--- util/utils.py
+++ util/utils.py
@@ -23,7 +23,7 @@ reader = easyocr.Reader(['en'])
 paddle_ocr = PaddleOCR(
     lang='en',  # other lang also available
     use_angle_cls=False,
-    max_batch_size=1024,
+    # max_batch_size=1024,
     use_dilation=True,  # improves accuracy
     rec_batch_num=1024)
 import time
